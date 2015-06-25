840de5b327c109c830dd0c90ffc6df5e1ec58541
leMingathon <smartsir123@gmail.com>
2015-06-25 17:31:21 +0100

    More FXML, and finally started using SceneBuilder!
    I've got to admit, it IS pretty cool.

--- src/randomtesting/Controller2.java
+++ src/randomtesting/Controller2.java
@@ -1,13 +1,15 @@
 package randomtesting;
 
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller2 {
-    @FXML
-    private Button button;
+    public Label daLabel;
+    public TextField textField;
+    public Button button;
 
     public void dealWithIt() {
-        button.setText("You clicked me!!!");
+        daLabel.setText("Yo! You clicked the button!!!");
     }
 }
